3217096b2cbb4641b741c854d128785beedc6d09
Alex <alexkmartinez77@gmail.com>
2018-11-07 21:34:43 -0800

    uses the bootstrap grid system as an alternative to display CityInfo vs table rows

--- src/js/components/CityInfo/CityInfo.jsx
+++ src/js/components/CityInfo/CityInfo.jsx
@@ -9,44 +9,87 @@ export default class CityInfo extends React.Component {
             <div className='card border-primary mb-3'>
                 <div className='card-header text-white bg-primary'>City Information</div>
                 <div className='card-body'>
-                    <div className="container">
-                        <table className='text-center'>
-                            <tbody className='text-center'>
-                                <tr className='text-center'>
-                                    <td colSpan='3' className='text-center'><h2>{location}</h2></td>
-                                </tr>
-                                <tr>
-                                    <td colSpan='3' className='text-center'>Latitude/Longitude: {lat}, {lon} </td>
-                                </tr>
-                                <tr className='text-center'>
-                                    <th className='text-center'>Temperature (F)</th>
-                                    <th className='text-center'>Pressure</th>
-                                    <th className='text-center'>Humidity</th>
-                                </tr>
-
-                                <tr className='text-success text-center'>
-                                    <td className='text-center'>{temp}</td>
-                                    <td className='text-center'>{press}</td>
-                                    <td className='text-center'>{hum}</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr className='text-center'>
-                                    <th className='text-center'>Lowest Temp (F)</th>
-                                    <th className='text-center'>Highest Temp (F)</th>
-                                    <th className='text-center'>Wind Speed</th>
-                                </tr>
-                                <tr className='text-success text-center'>
-                                    <td className='text-center'>{low}</td>
-                                    <td className='text-center'>{high}</td>
-                                    <td className='text-center'>{wind}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <h5 className="card-title">{location}</h5>
+                    <p>
+                        <strong>Latitude/Longitude: {lat}, {lon}</strong>
+                    </p>
+                    <hr />
+                    <div className="row">
+                        <div className="col-4">
+                            <p>
+                                <strong>Temperature (F)</strong>
+                            </p>
+                            <p className="text-success">{temp}</p>
+                        </div>
+                        <div className="col-4">
+                            <p>
+                                <strong>Pressure</strong>
+                            </p>
+                            <p className="text-success">{press}</p>
+                        </div>
+                        <div className="col-4">
+                            <p>
+                                <strong>Humidity</strong>
+                            </p>
+                            <p className="text-success">{hum}</p>
+                        </div>
                     </div>
+                    <div className="row">
+                        <div className="col-4">
+                            <p>
+                                <strong>Lowest Temp (F)</strong>
+                            </p>
+                            <p className="text-success">{low}</p>
+                        </div>
+                        <div className="col-4">
+                            <p>
+                                <strong>Highest Temp (F)</strong>
+                            </p>
+                            <p className="text-success">{high}</p>
+                        </div>
+                        <div className="col-4">
+                            <p>
+                                <strong>Wind Speed</strong>
+                            </p>
+                            <p className="text-success">{wind}</p>
+                        </div>
+                    </div>
+                    {/* <table>
+                        <tbody>
+                            <tr>
+                                <td colSpan='3' className='text-center'><h2>{location}</h2></td>
+                            </tr>
+                            <tr>
+                                <td colSpan='3' className='text-center'>Latitude/Longitude: {lat}, {lon} </td>
+                            </tr>
+                            <tr>
+                                <th className='text-center'>Temperature (F)</th>
+                                <th className='text-center'>Pressure</th>
+                                <th className='text-center'>Humidity</th>
+                            </tr>
+
+                            <tr className='text-success'>
+                                <td className='text-center'>{temp}</td>
+                                <td className='text-center'>{press}</td>
+                                <td className='text-center'>{hum}</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th className='text-center'>Lowest Temp (F)</th>
+                                <th className='text-center'>Highest Temp (F)</th>
+                                <th className='text-center'>Wind Speed</th>
+                            </tr>
+                            <tr className='text-success'>
+                                <td className='text-center'>{low}</td>
+                                <td className='text-center'>{high}</td>
+                                <td className='text-center'>{wind}</td>
+                            </tr>
+                        </tbody>
+                    </table> */}
                 </div>
             </div>
         );
--- src/js/components/SearchHx/SearchHx.jsx
+++ src/js/components/SearchHx/SearchHx.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 
-export default class SearchHx extends React.Component{
+export default class SearchHx extends React.Component {
 
-    render(){
+    render() {
         const { searchHistory } = this.props;
-        return(
+        return (
             <div className='card border-primary mb-3'>
-            <div className='card-header text-white bg-primary'>Search History</div>
-            <div className='card-body'>
-            <table className="table table-striped">
-                <tbody>
-               { searchHistory.map((searched) =>
-                <tr>
-                        <td>{searched.location}</td>
-                        <td className='text-right'>{searched.date}     {searched.time}</td>
-                </tr> 
-                )
-            }
-                </tbody>
-            </table>
+                <div className='card-header text-white bg-primary'>Search History</div>
+                <div className='card-body'>
+                    {/* {searchHistory.map((searched) =>
+                            <div className='row'>
+                                <div className='col-4'><p>{searched.location}</p></div>
+                                <div className='col-8 text-right'><p>{searched.date} {searched.time}</p></div>
+                            </div>
+
+                        )
+                        } */}
+                    <table className="table table-striped">
+                        <tbody>
+                            {searchHistory.map((searched) =>
+                                <tr>
+                                    <td>{searched.location}</td>
+                                    <td className='text-right'>{searched.date}     {searched.time}</td>
+                                </tr>
+                            )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
-          </div>
         );
     }
-
 }
